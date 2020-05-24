74b02027a863efbeec61d325df05899df65f089c
Adrian Kryzinski <adriankryzinski@gmail.com>
2020-05-24 08:58:27 +0200

    Remove unnecessery code

--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -1,8 +1,7 @@
 import shortid from 'shortid';
 
 // selectors
-// export const getCardsForColumn = ({cards, searchString}, columnId) => cards.filter(card => card.columnId == columnId && new RegExp(searchString, 'i').test(card.title));
-export const getCardsForColumn = ({ cards }, columnId) => cards.filter(card => card.columnId == columnId); // <--- removing a code fragment, responsible for filtering cards according to the searched phrase
+export const getCardsForColumn = ({ cards }, columnId) => cards.filter(card => card.columnId == columnId); 
 export const getCardsForSearch = ({ cards, columns, lists} , searchString) => cards.filter(card => new RegExp(searchString, 'i').test(card.title)).map(card => ({
   ...card,
   listId: lists.find(list => list.id === columns.find(col => col.id === card.columnId).listId).id,
