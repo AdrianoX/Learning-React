fb7758e987598ac470ca33ac389089d011d21807
Adrian Kryzinski <adriankryzinski@gmail.com>
2020-06-15 23:40:27 +0200

    Test - problem with git

--- src/index.js
+++ src/index.js
@@ -8,4 +8,3 @@ import store from './redux/store';
 
 // ReactDOM.render(<App />, document.getElementById('app'));
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('app'));
-
